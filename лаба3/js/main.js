a550cc37ba0d0ae836490ff7e8940698bffb8314
6Fillin9 <113698544+6Fillin9@users.noreply.github.com>
2023-09-24 23:09:01 +0300

    1 по 4 лабы

---
+++ лаба3/js/main.js
@@ -0,0 +1,7 @@
+const burgerMenu = document.querySelector('.burgerMenu');
+    const handleBurgerClick = (evt) => {
+      evt.preventDefault();
+      burgerMenu.classList.toggle('active');
+    }
+   
+    burgerMenu.addEventListener('click', handleBurgerClick);
